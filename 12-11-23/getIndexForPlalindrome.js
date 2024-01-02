7259f157ac0ff3b4b2c354d93bd0bfe60f87bef7
sam <sam@mitchs-MBP.anderson.local>
2024-01-02 09:24:16 -0700

    add all prompts

---
+++ 12-11-23/getIndexForPlalindrome.js
@@ -0,0 +1,22 @@
+// In Santa's workshop, the elves love puzzles 🧠. This year, they have created a special one: a challenge to form a Christmas palindrome.
+
+// A palindrome is a word that reads the same forwards and backwards. The elves want to know if it is possible to form a palindrome by making, at most, one exchange of letters.
+
+// Create a function getIndexsForPalindrome that receives a string and returns:
+
+// If it is already a palindrome, an empty array.
+// If it is not possible, null.
+// If a palindrome can be formed with one change, an array with the two positions (indexes) that must be swapped to create it. If the palindrome can be formed with different swaps, always return the first one found.
+
+// Examples
+
+// getIndexsForPalindrome('anna') // []
+// getIndexsForPalindrome('abab') // [0, 1]
+// getIndexsForPalindrome('abac') // null
+// getIndexsForPalindrome('aaaaaaaa') // []
+// getIndexsForPalindrome('aaababa') // [1, 3]
+// getIndexsForPalindrome('caababa') // null
+
+function getIndexsForPalindrome(word) {
+  return [0, 0]
+}
